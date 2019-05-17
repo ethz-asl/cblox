59542fc455d153c8b6b4d0b1127f6f0abfb0621e
Laura <gasserl@student.ethz.ch>
2019-05-17 16:00:14 +0200

    backup before converting to only templated submap server

--- cblox_ros/src/tsdf_submap_server_node.cc
+++ cblox_ros/src/tsdf_submap_server_node.cc
@@ -1,7 +1,7 @@
 #include <glog/logging.h>
 #include <ros/ros.h>
-#include <cblox_ros/tsdf_submap_server.h>
-//#include <cblox_ros/submap_server_template.h>
+//#include <cblox_ros/tsdf_submap_server.h>
+#include <cblox_ros/submap_server_template.h>
 
 int main(int argc, char** argv) {
   ros::init(argc, argv, "cblox");
@@ -11,8 +11,8 @@ int main(int argc, char** argv) {
   ros::NodeHandle nh;
   ros::NodeHandle nh_private("~");
 
-  //cblox::SubmapServer<cblox::TsdfEsdfSubmap> node(nh, nh_private);
-  cblox::TsdfSubmapServer node(nh, nh_private);
+  cblox::SubmapServer<cblox::TsdfEsdfSubmap> node(nh, nh_private);
+  //cblox::TsdfSubmapServer node(nh, nh_private);
 
   ros::spin();
   return 0;
