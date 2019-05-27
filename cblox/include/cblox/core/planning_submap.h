68979b058ccbe17b611e18733ddfd6e15170c72f
Laura <gasserl@student.ethz.ch>
2019-05-27 14:02:01 +0200

    proper map bounds calculation

--- cblox/include/cblox/core/planning_submap.h
+++ cblox/include/cblox/core/planning_submap.h
@@ -1,6 +1,7 @@
 #ifndef CBLOX_PLANNING_SUBMAP_H
 #define CBLOX_PLANNING_SUBMAP_H
 
+#include <cblox/core/tsdf_esdf_submap.h>
 #include <voxblox/utils/planning_utils.h>
 #include <voxblox_skeleton/skeleton.h>
 #include <voxblox_skeleton/skeleton_generator.h>
@@ -21,46 +22,13 @@ public:
         skeleton_generator_(esdf_map_->getEsdfLayerPtr()) { }
 
   // map functions
-  void computeMapBounds() {
-    Eigen::Vector3d lower_tmp, upper_tmp;
-    voxblox::utils::computeMapBoundsFromLayer(*tsdf_map_->getTsdfLayerPtr(),
-        &lower_tmp, &upper_tmp);
-
-    // get corners of bounding cube
-    std::vector<Eigen::Vector3d> bounds;
-    bounds.emplace_back(lower_tmp);
-    bounds.emplace_back(upper_tmp);
-    bounds.emplace_back(Eigen::Vector3d(
-        lower_tmp.x(), lower_tmp.y(), upper_tmp.z()));
-    bounds.emplace_back(Eigen::Vector3d(
-        lower_tmp.x(), upper_tmp.y(), lower_tmp.z()));
-    bounds.emplace_back(Eigen::Vector3d(
-        upper_tmp.x(), lower_tmp.y(), lower_tmp.z()));
-    bounds.emplace_back(Eigen::Vector3d(
-        lower_tmp.x(), upper_tmp.y(), upper_tmp.z()));
-    bounds.emplace_back(Eigen::Vector3d(
-        upper_tmp.x(), lower_tmp.y(), upper_tmp.z()));
-    bounds.emplace_back(Eigen::Vector3d(
-        upper_tmp.x(), upper_tmp.y(), lower_tmp.z()));
-
-    // transform and save global extrema
-    bool first = true;
-    for (int i = 0; i < bounds.size(); i++) {
-      bounds[i] = T_M_S_.cast<double>() * bounds[i];
-      if (first) {
-        first = false;
-        lower_bound_ = bounds[i];
-        upper_bound_ = bounds[i];
-      } else {
-        lower_bound_ = bounds[i].cwiseMin(lower_bound_);
-        upper_bound_ = bounds[i].cwiseMax(upper_bound_);
-      }
-    }
-  }
+  void computeMapBounds();
   void getMapBounds(Eigen::Vector3d* lower_bound, Eigen::Vector3d* upper_bound) {
     *lower_bound = lower_bound_;
     *upper_bound = upper_bound_;
   }
+  void getGlobalMapBounds(Eigen::Vector3d* lower_bound,
+                          Eigen::Vector3d* upper_bound);
 
   // skeleton functions
   void generateSkeleton() {
@@ -74,33 +42,6 @@ public:
   voxblox::SkeletonGenerator* getSkeletonGenerator() {
     return &skeleton_generator_;
   }
-  /*
-  voxblox::SparseSkeletonGraph getGlobalSparseGraph() {
-    voxblox::SparseSkeletonGraph local_graph =
-        skeleton_generator_.getSparseGraph();
-    voxblox::SparseSkeletonGraph global_graph = local_graph;
-    ROS_INFO_STREAM("before transform: [" << local_graph.getVertex(0).point.x()
-        << ", " << local_graph.getVertex(0).point.y()
-        << ", " << local_graph.getVertex(0).point.z() << "]");
-    global_graph.transformFrame(T_M_S_);
-    ROS_INFO_STREAM("after transform:  " << local_graph.getVertex(0).point.x()
-        << ", " << local_graph.getVertex(0).point.y()
-        << ", " << local_graph.getVertex(0).point.z() << "]");
-    ROS_INFO_STREAM("after transform:  " << global_graph.getVertex(0).point.x()
-        << ", " << global_graph.getVertex(0).point.y()
-        << ", " << global_graph.getVertex(0).point.z() << "]");
-    ROS_INFO_STREAM(T_M_S_);
-    return global_graph;
-  }
-  */
-  /*
-  voxblox::Skeleton getSkeletonLayerPtr() {
-    return skeleton_generator_.getSkeleton();
-  }
-  voxblox::SparseSkeletonGraph getSparseGraph() {
-    return skeleton_generator_.getSparseGraph();
-  }
-  */
 
  private:
   Eigen::Vector3d lower_bound_;
---
+++ cblox/src/core/planning_submap.cpp
@@ -0,0 +1,45 @@
+#include "cblox/core/planning_submap.h"
+
+namespace cblox {
+
+void PlanningSubmap::computeMapBounds() {
+  Eigen::Vector3d lower_tmp, upper_tmp;
+  voxblox::utils::computeMapBoundsFromLayer(*tsdf_map_->getTsdfLayerPtr(),
+                                            &lower_bound_, &upper_bound_);
+}
+
+void PlanningSubmap::getGlobalMapBounds(Eigen::Vector3d* lower_bound,
+                        Eigen::Vector3d* upper_bound) {
+    // get corners of bounding cube
+    std::vector<Eigen::Vector3d> bounds;
+    bounds.emplace_back(lower_bound_);
+    bounds.emplace_back(upper_bound_);
+    bounds.emplace_back(Eigen::Vector3d(
+        lower_bound_.x(), lower_bound_.y(), upper_bound_.z()));
+    bounds.emplace_back(Eigen::Vector3d(
+        lower_bound_.x(), upper_bound_.y(), lower_bound_.z()));
+    bounds.emplace_back(Eigen::Vector3d(
+        upper_bound_.x(), lower_bound_.y(), lower_bound_.z()));
+    bounds.emplace_back(Eigen::Vector3d(
+        lower_bound_.x(), upper_bound_.y(), upper_bound_.z()));
+    bounds.emplace_back(Eigen::Vector3d(
+        upper_bound_.x(), lower_bound_.y(), upper_bound_.z()));
+    bounds.emplace_back(Eigen::Vector3d(
+        upper_bound_.x(), upper_bound_.y(), lower_bound_.z()));
+
+    // transform and save global extrema
+    bool first = true;
+    for (int i = 0; i < bounds.size(); i++) {
+      bounds[i] = T_M_S_.cast<double>() * bounds[i];
+      if (first) {
+        first = false;
+        *lower_bound = bounds[i];
+        *upper_bound = bounds[i];
+      } else {
+        *lower_bound = bounds[i].cwiseMin(*lower_bound);
+        *upper_bound = bounds[i].cwiseMax(*upper_bound);
+      }
+    }
+  }
+
+} // namespace cblox
