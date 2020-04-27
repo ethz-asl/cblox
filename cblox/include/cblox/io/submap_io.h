401d5c5d67d70157862ce431abf7a7baa52c78a2
Alex Millane <alex.millane@gmail.com>
2020-04-27 09:57:12 +0200

    Removed now empty loading implementation file.

--- cblox/include/cblox/io/submap_io.h
+++ cblox/include/cblox/io/submap_io.h
@@ -14,15 +14,18 @@ namespace io {
 template <typename SubmapType>
 bool LoadSubmapCollection(
     const std::string& file_path,
-    typename SubmapCollection<SubmapType>::Ptr* tsdf_submap_collection_ptr);
+    typename SubmapCollection<SubmapType>::Ptr* submap_collection_ptr) {
+  return SubmapCollection<SubmapType>::LoadFromFile(file_path,
+                                                    submap_collection_ptr);
+}
 
 template <typename SubmapType>
 bool SaveSubmapCollection(const SubmapCollection<SubmapType>& submap_collection,
-                          const std::string& file_path);
+                          const std::string& file_path) {
+  return submap_collection.saveToFile(file_path);
+}
 
 }  // namespace io
 }  // namespace cblox
 
 #endif  // CBLOX_IO_SUBMAP_IO_H_
-
-#include "cblox/io/submap_io_inl.h"
